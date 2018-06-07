d4e3e6abc1fb40b52380f9d92ece718b5f619691
Ivan Shcherbak <alotofall@gmail.com>
2018-06-07 09:08:13 +0300

    #9 fix code style

--- src/ImmediateExceptionPrinter.php
+++ src/ImmediateExceptionPrinter.php
@@ -3,15 +3,15 @@ namespace ScriptFUSION\PHPUnitImmediateExceptionPrinter;
 
 
 if (class_exists(\PHPUnit_TextUI_ResultPrinter::class)) {
-  class ImmediateExceptionPrinter extends PhpUnit5Printer {
-
-  }
-} elseif (class_exists(\PHPUnit\Runner\Version::class) && strpos('7.', \PHPUnit\Runner\Version::id()) === 0) {
+    class ImmediateExceptionPrinter extends PhpUnit5Printer
+    {
+    }
+} elseif (class_exists(\PHPUnit\Runner\Version::class) && strpos(\PHPUnit\Runner\Version::id(), '7.') === 0) {
     class ImmediateExceptionPrinter extends PhpUnit7Printer
     {
     }
 } else {
-  class ImmediateExceptionPrinter extends PhpUnit6Printer {
-
-  }
+    class ImmediateExceptionPrinter extends PhpUnit6Printer
+    {
+    }
 }
