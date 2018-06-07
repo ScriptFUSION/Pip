832534c3952695ce6abfd1885b815b3c7cdf531f
Ivan Shcherbak <alotofall@gmail.com>
2018-06-07 09:02:16 +0300

    Try to implement phpunit 7 support

--- src/ImmediateExceptionPrinter.php
+++ src/ImmediateExceptionPrinter.php
@@ -1,12 +1,17 @@
 <?php
 namespace ScriptFUSION\PHPUnitImmediateExceptionPrinter;
 
+
 if (class_exists(\PHPUnit_TextUI_ResultPrinter::class)) {
-    class ImmediateExceptionPrinter extends PhpUnit5Printer
+  class ImmediateExceptionPrinter extends PhpUnit5Printer {
+
+  }
+} elseif (class_exists(\PHPUnit\Runner\Version::class) && strpos('7.', \PHPUnit\Runner\Version::id()) === 0) {
+    class ImmediateExceptionPrinter extends PhpUnit7Printer
     {
     }
 } else {
-    class ImmediateExceptionPrinter extends PhpUnit6Printer
-    {
-    }
+  class ImmediateExceptionPrinter extends PhpUnit6Printer {
+
+  }
 }
---
+++ src/PhpUnit7Printer.php
@@ -0,0 +1,42 @@
+<?php
+namespace ScriptFUSION\PHPUnitImmediateExceptionPrinter;
+
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\Test;
+use PHPUnit\TextUI\ResultPrinter;
+
+class PhpUnit7Printer extends ResultPrinter
+{
+    use Printer;
+
+    public function startTest(Test $test) : void
+    {
+        parent::startTest($test);
+
+        $this->onStartTest();
+    }
+
+    protected function writeProgressWithColor(string $color, string $buffer): void
+    {
+        parent::writeProgressWithColor($color, $buffer);
+
+        $this->onWriteProgressWithColor($color);
+    }
+
+    public function endTest(Test $test, float $time): void
+    {
+        parent::endTest($test, $time);
+
+        $this->onEndTest($test, $time);
+    }
+
+    public function addError(Test $test, \Throwable $t, float $time): void
+    {
+        $this->onAddError(new \Exception($t->getMessage(), $t->getCode(), $t));
+    }
+
+    public function addFailure(Test $test, AssertionFailedError $e, float $time): void
+    {
+        $this->onAddFailure($e);
+    }
+}
