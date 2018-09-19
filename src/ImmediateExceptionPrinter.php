542d98a0f858658a2b63c2265dce302d5daa1920
oshdev <karol.slomczynski@gmail.com>
2018-09-19 16:13:30 +0100

    Fix

--- src/ImmediateExceptionPrinter.php
+++ src/ImmediateExceptionPrinter.php
@@ -1,12 +1,21 @@
 <?php
 namespace ScriptFUSION\PHPUnitImmediateExceptionPrinter;
 
-if (class_exists(\PHPUnit_TextUI_ResultPrinter::class)) {
+use PHPUnit_TextUI_ResultPrinter;
+use PHPUnit\Runner\Version;
+use function class_exists;
+use function version_compare;
+
+if (class_exists(PHPUnit_TextUI_ResultPrinter::class)) {
     class ImmediateExceptionPrinter extends PhpUnit5Printer
     {
     }
-} else {
+} elseif (class_exists(Version::class) && version_compare(Version::id(), '7.0', '<=')) {
     class ImmediateExceptionPrinter extends PhpUnit6Printer
+    {
+    }
+} else {
+    class ImmediateExceptionPrinter extends PhpUnit7Printer
     {
     }
 }
