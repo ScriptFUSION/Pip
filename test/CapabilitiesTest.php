fa10e64ef5b2ac2bb55b2bd4ecb60020dd78692e
Álvaro González <alvaro@demogracia.com>
2024-10-22 11:32:55 +0200

    Tested that duplicate warnings are reported once.

--- test/CapabilitiesTest.php
+++ test/CapabilitiesTest.php
@@ -98,6 +98,34 @@ final class CapabilitiesTest extends TestCase
         self::assertTrue(true);
     }
 
+    private function triggerWarning(): void
+    {
+        foreach (1 as $n) {}
+    }
+
+    public function testDuplicateWarningsA(): void
+    {
+        $this->triggerWarning();
+
+        self::assertTrue(true);
+    }
+
+    public function testDuplicateWarningsB(): void
+    {
+        $this->triggerWarning();
+        $this->triggerWarning();
+        $this->triggerWarning();
+
+        foreach (1 as $n) {}
+
+        self::assertTrue(true);
+    }
+
+    public function testDuplicateWarningsC(): void
+    {
+        self::assertTrue(true);
+    }
+
     #[DataProvider('provideData')]
 
     public function testDataProvider(): void
